0b87b46c08ff11be3716ed9c49dc368d7dd12dec
hunterbrightdesign <70662742+hunterbrightdesign@users.noreply.github.com>
2025-01-22 22:40:55 +0100

    rm venv

--- venv/lib/python3.11/site-packages/shedskin/lib/functools.py
+++
@@ -1,5 +0,0 @@
-def reduce(func, iter1, init=None):
-    elem = iter(iter1).__next__()
-#    elem = init
-    elem = func(elem, elem)
-    return elem
--- venv/lib/python3.11/site-packages/shedskin/lib/string.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2005-2022 Mark Dufour and contributors; License Expat (See LICENSE)
-
-
-ascii_letters = ascii_lowercase = ascii_uppercase = whitespace = printable = punctuation = ''
-digits = hexdigits = octdigits = ''
-
-def capwords(s, sep=''):
-    return ''
