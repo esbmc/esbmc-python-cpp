f6efb600bff04655b3c4412db5f0988e45ac70c3
hunterbrightdesign <70662742+hunterbrightdesign@users.noreply.github.com>
2025-02-03 23:03:21 +0100

    stash dict

--- examples/example_15_dictionary.py
+++ examples/example_15_dictionary.py
@@ -1,11 +1,22 @@
+def test_pop_from_dict():
+    # Create a dictionary
+    
 
-my_dict = {"a": 1, "b": 2, "c": True, "d": False,"e": "abc","f": "jwt"}
-assert (my_dict["a"] == 1)
-assert (my_dict["c"] == True)
-assert (my_dict["f"] == "jwt")
+    # Pop a key-value pair from the dictionary
+    # value = my_dict.pop("b", -1)
+    # del my_dict["b"]
+    # Assert the value is as expected
+    # assert value == 2, "The popped value should be 2"
+    # print(my_dict)
+    # Assert the key is no longer in the dictionary
+    # assert "b" not in my_dict, "The key 'b' should no longer be in the dictionary"
 
-del my_dict["b"]
-assert "b" not in my_dict
+    my_dict = {"a": 1, "b": 2, "c": 3}
+    assert (my_dict["a"] == 1)
+    del my_dict["b"]
+    assert "b" not in my_dict
 
+# Run the test
+test_pop_from_dict()
 
-
+print("Test passed!")
