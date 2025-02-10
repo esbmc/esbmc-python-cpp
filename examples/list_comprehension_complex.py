2050b2bdbeeff7aca28daba82bdf33792e999dfe
Shivkumar Shivaji <sshivaji@gmail.com>
2025-02-10 11:59:54 -0800

    fix examples

--- examples/list_comprehension_complex.py
+++ examples/list_comprehension_complex.py
@@ -28,7 +28,6 @@ def main():
     actions = [Down(), Up()]
     while True:
         enabled_actions = list_comp(actions, lambda a: a.pre())
-        assert 0 == 1
         if enabled_actions:
             length = len(enabled_actions)
             action_nr = random.randint(0, length-1)
