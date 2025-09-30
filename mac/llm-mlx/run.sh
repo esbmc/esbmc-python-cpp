100f3987fe1a0206ecbc664d85aba9d93220440f
Shivkumar Shivaji <sshivaji@gmail.com>
2025-09-30 10:51:19 -0700

    Latest 4.6 model

--- mac/llm-mlx/run.sh
+++ mac/llm-mlx/run.sh
@@ -2,5 +2,5 @@
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
-mlx_lm.server --model mlx-community/GLM-4.5-Air-4bit --port 8080 --max-tokens 32000 --chat-template-args '{"enable_thinking":true}'
+mlx_lm.server --model mlx-community/GLM-4.6-4bit --port 8080 --max-tokens 32000 --chat-template-args '{"enable_thinking":true}'
 #mlx_lm.server --model mlx-community/Qwen2.5-7B-Instruct-4bit --port 8080 --max-tokens 32000 --chat-template-args '{"enable_thinking":true}'
